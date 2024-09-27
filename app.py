b956633c95bb41fb9f27254507fc98ea861f7acc
jfonseca1 <33073949+jfonseca1@users.noreply.github.com>
2024-09-27 19:35:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,2 @@
-# Python file (app.py)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return '''
-<html>
-<body>
-<center>
-<h1>Demo on GitOps with ArgoCD and Github Actions.</h1> <br>
-<br>
-<img src="https://github.com/jfonseca1/CICD_Application_K8s/blob/main/itsworking.jpeg?raw=true">
-</center>
-</body>
-</html>
-'''
+if __name__ == '__main__':
+    print('Hello Docker world!')
