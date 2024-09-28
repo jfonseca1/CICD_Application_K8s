671bb5d25037b42f142bc5e4566cdc721a4004d2
jfonseca1 <33073949+jfonseca1@users.noreply.github.com>
2024-09-28 22:53:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
-# Use the official Python 3.10 image as the base image
-FROM python:3.10
+from flask import Flask, render_template
 
-# Set the working directory inside the container
-WORKDIR /app
+app = Flask(__name__)
 
-# Copy the requirements file to the working directory
-COPY requirements.txt .
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# Install the Python dependencies
-RUN pip install --no-cache-dir -r requirements.txt
-
-# Copy the rest of the application code to the working directory
-COPY . .
-
-# Expose the port on which the Flask app will run
-EXPOSE 5000
-
-# Specify the command to run the application
-CMD [ "python", "app.py" ]
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
