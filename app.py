ab6554d451fcd53ed34d0774254d28ef64ae4df1
jfonseca1 <33073949+jfonseca1@users.noreply.github.com>
2024-09-29 01:25:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
-
+# Python file (app.py)
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def hello_world():
+    return '''
+<html>
+<body>
+<center>
+<h1>Demo on GitOps with ArgoCD and Github Actions.</h1> <br>
+<br>
+<img src="https://github.com/tanmaybhandge/CICD_Application_K8s/blob/main/itsworking.jpeg?raw=true">
+</center>
+</body>
+</html>
+'''
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
